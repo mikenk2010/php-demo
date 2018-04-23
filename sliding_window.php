9a62c0322b514781f51c6766fd09d76ac40675a1
Bao Nguyen <baonguyen.work@gmail.com>
2018-04-23 12:33:07 +0700

    Add raw_method2 to cal find max, better for large number

--- sliding_window.php
+++ sliding_window.php
@@ -5,15 +5,23 @@
 
 /**
  * @param $num array
- * @param $k integer
+ * @param $k string
+ * @return array
  */
 function slidingWindow($num, $k) {
   return [
     "Raw method" => json_encode(raw_method($num, $k)),
+    "Raw method2" => json_encode(raw_method2($num, $k)),
     "build-in" => json_encode(build_in_method($num, $k)),
   ];
 }
 
+/**
+ *
+ * @param $nums array
+ * @param $k string
+ * @return array
+ */
 function raw_method($nums, $k) {
   $start = recordStart();
 
@@ -31,6 +39,39 @@ function raw_method($nums, $k) {
   $end = recordEnd($start);
 
   return ['result raw_method' => $result, 'input' => $nums, 'slide' => $k, 'total exe time (microsecond)' => $end];
+}
+
+/**
+ * Compare left and right
+ * Result: it's slower than compare from left to right, rather than merge left and right then compare
+ * @param $nums array
+ * @param $k string
+ * @return array
+ */
+function raw_method2($nums, $k) {
+  $start = recordStart();
+  $result = [];
+
+  $max_left[0] = $nums[0];
+  $max_right[count($nums) - 1] = $nums[count($nums) - 1];
+
+  // Separate max start from left and max start from right
+  for ($i = 0; $i < count($nums); $i++) {
+    $max_left[$i] = ($i % $k == 0) ? $nums[$i] : max($max_left[$i - 1], $nums[$i]);
+
+    $j = count($nums) - $i - 1;
+    $max_right[$j] = ($j % $k == 0) ? $nums[$j] : max(isset($max_right[$j + 1]) ? $max_right[$j + 1] : $max_right[$j], $nums[$j]);
+  }
+
+  // Merge and compare
+  for ($i = 0, $j = 0; $i + $k <= count($nums); $i++) {
+    $result[$j++] = max($max_right[$i], $max_left[$i + $k - 1]);
+  }
+
+
+  $end = recordEnd($start);
+
+  return ['result raw_method2' => $result, 'input' => $nums, 'slide' => $k, 'total exe time (microsecond)' => $end];
 }
 
 function build_in_method($nums, $k) {
